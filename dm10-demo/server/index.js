3ab84a9d0eb74f5646584b3363e4bca1c3ac7a46
ericdrewes <eric.d.drewes@gmail.com>
2018-01-08 13:58:34 -0600

    backend working

--- dm10-demo/server/index.js
+++ dm10-demo/server/index.js
@@ -1,83 +1,93 @@
-require('dotenv').config();
-const express = require('express');
-const {json} = require('body-parser');
-const cors = require('cors');
-const session = require('express-session');
-const massive = require('massive');
-const passport  = require('passport');
-const Auth0Strategy = require('passport-auth0');
+require("dotenv").config();
+const express = require("express");
+const { json } = require("body-parser");
+const cors = require("cors");
+const session = require("express-session");
+const massive = require("massive");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
 
 massive(process.env.CONNECTION_STRING)
-    .then(db => {
-        app.set('db', db)
-    }) .catch(console.log)
+  .then(db => {
+    app.set("db", db);
+  })
+  .catch(console.log);
 
 const app = express();
 
 app.use(json());
 app.use(cors());
 app.use(
-    session({
-      secret: process.env.SESSION_SECRET,
-      resave: false,
-      saveUnitialized: false,
-      cookie: {
-          maxAge: 525600 * 60 * 1000
-      }
-    })
-  );
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUnitialized: false,
+    cookie: {
+      maxAge: 525600 * 60 * 1000
+    }
+  })
+);
 
-  app.use(passport.initialize());
-  app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
-  passport.use(new Auth0Strategy({
+passport.use(
+  new Auth0Strategy(
+    {
       domain: process.env.AUTH_DOMAIN,
       clientSecret: process.env.CLIENT_SECRET,
       clientID: process.env.CLIENT_ID,
-      callbackURL: '/auth'
-    }, 
+      callbackURL: "/auth"
+    },
     (accessToken, refreshToken, extraParams, profile, done) => {
-        app.get('db')
+      app
+        .get("db")
         .getUserByAuthId(profile.id)
         .then(response => {
-            if (!response[0]) { 
-                app.get('db').createUserByAuthId([profile.id, profile.displayName])
-                .then(created => {
-                    return done(null, created[0]);
-                })
-            }else {
-                return done(null, response[0])
-            }
-        })
-        return done(null, profile);
-    }));
+          if (!response[0]) {
+            app
+              .get("db")
+              .createUserByAuthId([profile.id, profile.displayName])
+              .then(created => {
+                return done(null, created[0]);
+              });
+          } else {
+            return done(null, response[0]);
+          }
+        });
+      return done(null, profile);
+    }
+  )
+);
 
-    passport.serializeUser((user, done) => done(null, user))
-    passport.deserializeUser((user, done) => done(null, user))
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
 
+app.get(
+  "/auth",
+  passport.authenticate("auth0", {
+    successRedirect: "http://localhost:3000/",
+    failureRedirect: "http://localhost:3000/auth"
+  })
+);
 
-    app.get('/auth', passport.authenticate('auth0', {
-        successRedirect: 'http://localhost:3000/',
-        failureRedirect: 'http://localhost:3000/auth'
-        })
-    );
+app.get("/api/me", (req, res, next) => {
+  if (req.user) res.json(req.user);
+  else res.redirect("/auth");
+});
 
-    app.get ('/api/me', (req, res, next) => {
-        if(req.user) res.json (req.user);
-        else res.redirect('/auth')
-    });
+app.get("/api/test", (req, res) => {
+  const db = app.get("db");
 
-  app.get('/api/test', (req, res) => {
-      const db = app.get('db');
-
-      db.products
-        .find({})
-        .then(response => {
-            console.log("backend")
-          res.json(response);
-      }).catch(console.log);
-  });
+  db.products
+    .find({})
+    .then(response => {
+      console.log("backend");
+      res.json(response);
+    })
+    .catch(console.log);
+});
 
 app.listen(process.env.PORT || 3001, () => {
-    console.log(`Listening on port: ${process.env.PORT || 3001}`)
-})
+  console.log(`Listening on port: ${process.env.PORT || 3001}`);
+});
